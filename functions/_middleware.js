4c7e5ae0c084e4f9635759bfc3d6a1b65dd9234f
Chopin <zhupan2009@gmail.com>
2023-07-06 22:42:49 +0800

    Update _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -11,11 +11,11 @@ function authentication(context) {
     return context.next();
   }
   if (context.request.headers.get("x-email") != "admin@example.com") {
-    const destinationURL = "https://example.com";
+    const destinationURL = "https://example.com"+encodeURIComponent(context.request.url);
     const statusCode = 301;
     return Response.redirect(destinationURL, statusCode);
     const headers = new Headers({"Set-Cookie": "name1=value1;Max-Age=300"});
-    let res = new Response("", { status: 301, headers:headers });
+    
     return res.redirect('https://www.oecom.cn/api/post');
   }
 
