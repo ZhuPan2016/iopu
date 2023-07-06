6e5ed72ac041e749550ca1e7403b5d4d5f7835db
Chopin <zhupan2009@gmail.com>
2023-07-07 00:15:42 +0800

    Update submit.js

--- functions/submit.js
+++ functions/submit.js
@@ -2,9 +2,20 @@ export async function onRequestPost(context) {
   try {
     let input = await context.request.formData();
     let pretty = JSON.stringify([...input], null, 2);
-    return new Response(pretty, {
+    let pretty = `<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>这是一个HTML5的网页</title>
+</head>
+<body>
+<p>Hello HTML5</p>
+</body>
+</html>`
+    return new Response(pretty2, {
       headers: {
         'Content-Type': 'application/json;charset=utf-8',
+        "Set-Cookie": "name1=value1;Max-Age=300"
       },
     });
   } catch (err) {
