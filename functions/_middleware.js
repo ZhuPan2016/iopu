06af86f6d9801a223c41b1ab449e065cf7e334c4
Chopin <zhupan2009@gmail.com>
2023-07-06 23:34:48 +0800

    Update _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -6,20 +6,6 @@ async function errorHandling(context) {
   }
 }
 
-function authentication(context) {
-  if (context.request.headers.get("Cookie") == "name1=value1") {
-    return context.next();
-  }
-  if (context.request.headers.get("x-email") != "admin@example.com") {
-    const destinationURL = "https://iopu.pages.dev/?id="+encodeURIComponent(context.request.url);
-    const statusCode = 301;
-    return Response.redirect(destinationURL, statusCode);
-    const headers = new Headers({"Set-Cookie": "name1=value1;Max-Age=300"});
-    
-    return res.redirect('https://www.oecom.cn/api/post');
-  }
 
-  return context.next();
-}
 
-export const onRequest = [errorHandling, authentication];
+export const onRequest = [errorHandling];
