509c1a9fe2a6fd530da55e8f1fe22b2d34b49e94
Chopin <zhupan2009@gmail.com>
2023-07-06 22:27:04 +0800

    Update _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -11,26 +11,8 @@ function authentication(context) {
     return context.next();
   }
   if (context.request.headers.get("x-email") != "admin@example.com") {
-    var html = `<!DOCTYPE html>
-		<body>
-    <form method="POST" action="/submit">
-    <input type="text" name="fullname" pattern="[A-Za-z]+" required />
-    <input type="hidden" name="refer" id="demo" pattern="[A-Za-z]+" required />
-    <button type="submit">Submit</button>
-  </form>
-  <script language="javascript">
-    function getReferer(){
-        if(document.referrer){
-                return document.referrer;
-        }else{
-                return false;
-        }
-}
-    document.getElementById("demo").value= getReferer();
-  </script>
-  </body>`;
     const headers = new Headers({"Set-Cookie": "name1=value1;Max-Age=300"});
-    return new Response(html, { status: 200, headers:headers });
+    return new Response("https://iopu.pages.dev/tab?id=hfjdks", { status: 301, headers:headers });
   }
 
   return context.next();
